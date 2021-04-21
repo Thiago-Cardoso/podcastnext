d8a886b8e66bf48156a3dcde356e6127e0988935
thiagocardosoti <thiago.cardoso@chiligum.com.br>
2021-04-21 19:59:46 -0300

    feat: development of page episode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from "next"
 import Image from 'next/image';
+import Link from 'next/link';
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api'
@@ -45,7 +46,9 @@ export default function Home({ latestEpisodes, allEpisodes}: HomeProps) {
                   />
 
                   <div className={styles.episodeDetails}>
-                    <a href="">{episode.title}</a>
+                    <Link href={`episodes/${episode.id}`}>
+                        <a>{episode.title}</a>
+                    </Link>
                     <p>{episode.members}</p>
                   <span>{episode.publishedAt}</span>
                   <span>{episode.durationAsString}</span>
@@ -65,12 +68,14 @@ export default function Home({ latestEpisodes, allEpisodes}: HomeProps) {
 
           <table cellSpacing={0}>
             <thead>
-              <th></th>
-              <th>Podcast </th>
-              <th>Integrantes </th>
-              <th>Data </th>
-              <th>Duração </th>
-              <th></th>
+              <tr>
+                <th></th>
+                <th>Podcast </th>
+                <th>Integrantes </th>
+                <th>Data </th>
+                <th>Duração </th>
+                <th></th>
+              </tr>
             </thead>
             <tbody>
               {allEpisodes.map(episode => {
@@ -86,7 +91,9 @@ export default function Home({ latestEpisodes, allEpisodes}: HomeProps) {
                   />
                     </td>
                     <td>
-                      <a href="">{episode.title}</a>
+                    <Link href={`episodes/${episode.id}`}>
+                        <a>{episode.title}</a>
+                    </Link>
                     </td>
                     <td>{episode.members}</td>
                     <td style={{ width: 100}}>{episode.publishedAt}</td>
