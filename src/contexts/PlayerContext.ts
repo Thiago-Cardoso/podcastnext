d9843108791189339e27b2adb25e4cf0ea1b3c4e
thiagocardosoti <thiago.cardoso@chiligum.com.br>
2021-04-23 00:23:51 -0300

    feat: development of events of play and pause

--- src/contexts/PlayerContext.ts
+++ src/contexts/PlayerContext.ts
@@ -11,7 +11,10 @@ type Episode = {
 type PlayerContextData = {
   episodeList: Episode[];
   currentEpisodeIndex: number;
+  isPlaying: boolean;
   play: (episode: Episode) => void;
+  setPlayingState: (state: boolean) => void;
+  tooglePlay: () => void;
 }
 
 export const PlayerContext = createContext({} as PlayerContextData);
